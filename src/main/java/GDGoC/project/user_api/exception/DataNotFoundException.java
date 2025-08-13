7fa9e637b547b3b332299158ee3a48d76fb760ae
acleruiys <acleruiys@gmail.com>
2025-08-13 17:01:14 +0900

    [Feat] DataNotFoundException

---
+++ src/main/java/GDGoC/project/user_api/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package GDGoC.project.user_api.exception;
+
+public class DataNotFoundException extends RuntimeException {
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+}
